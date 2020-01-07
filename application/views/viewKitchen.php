41a4197e4a0adca785d991c984a46280fee132cc
yurizza <44933955+yurizza@users.noreply.github.com>
2020-01-07 14:29:58 +0700

    Add files via upload

---
+++ application/views/viewKitchen.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>kitchen</title>
+  </head>
+  <body>
+      <!-- navbar -->
+   
+      <nav class="navbar navbar-dark bg-primary">
+          <h5 style="color:white">KITCHEN DISPLAY</h5>
+        </nav>
+        
+          <!-- akhir navbar -->
+
+          <div class="list-group">
+      <a href="<?=base_url('Kitchen/orderDetail')?>" class="list-group list-group-item-action">
+      <div class="row mt-3">
+              <div class="col my-auto ml-4">
+                <img class="img-fluid" height="70px" width="70px" src="<?=base_url('img/a.jfif')?>" alt="">
+              </div>
+              <div class="col">
+              <ul class="list-group">
+                  <li class="list-group">no order</li>
+                  <li class="list-group">tanggal</li>
+                  <li class="list-group">jam order</li>
+                </ul>
+              </div>
+              <div class="col my-auto">
+               <div >10 menit</div>
+              </div>
+          </div>
+      </a>
+      <div class="mx-3">
+      <hr>
+      </div>
+
+      <a href="<?=base_url('kitchen')?>" class="list-group list-group-item-action">
+      <div class="row mt-3">
+              <div class="col my-auto ml-4">
+                <img class="img-fluid" height="70px" width="70px" src="<?=base_url('img/a.jfif')?>" alt="">
+              </div>
+              <div class="col">
+              <ul class="list-group">
+                  <li class="list-group">no order</li>
+                  <li class="list-group">tanggal</li>
+                  <li class="list-group">jam order</li>
+                </ul>
+              </div>
+              <div class="col my-auto">
+               <div >10 menit</div>
+              </div>
+          </div>
+      </a>
+      <div class="mx-3">
+      <hr>
+      </div>
+      <a href="#" class="list-group list-group-item-action"><div class="row mt-3">
+              <div class="col my-auto ml-4">
+                <img class="img-fluid" height="70px" width="70px" src="<?=base_url('img/a.jfif')?>" alt="">
+              </div>
+              <div class="col">
+              <ul class="list-group">
+                  <li class="list-group">no order</li>
+                  <li class="list-group">tanggal</li>
+                  <li class="list-group">jam order</li>
+                </ul>
+              </div>
+              <div class="col my-auto">
+               <div >10 menit</div>
+              </div>
+          </div></a>
+    </div>
+    <div class="mx-3">
+      <hr>
+      </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <!-- <script src="<?=base_url('kitchen.js')?>"></script> -->
+  </body>
+</html>
---
+++ application/views/viewOrderDetail.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>kitchen</title>
+  </head>
+  <body>
+      <!-- navbar -->
+   
+      <nav class="navbar navbar-dark bg-primary">
+          <h5 style="color:white">ORDER DETAIL</h5>
+        </nav>
+        
+          <!-- akhir navbar -->
+
+          <div class="list-group">
+                <a class="list-group list-group-action">
+                <div class="row mt-3">
+                        <div class="col ml-3">
+                        <ul class="list-group">
+                            <li class="list-group">no order sklks sjs</li>
+                            <li class="list-group">tanggal</li>
+                            <li class="list-group">jam order</li>
+                            </ul>
+                        </div>
+                        <div class="col text-center my-auto">
+                        <div >10 menit yang lalu</div>
+                        </div>
+                    </div>
+                </a>
+         </div>
+         <div class="mt-3 mx-3">
+         <h5><font color="grey">MAKANAN</font></h5>
+            <div id="mk1" style="padding:5px" class="row mt-2">
+                <div class="col-8">
+                    <ul class="list-group">
+                        <li class="list-group">NASI GORENG HJJHJJ HJHJHJH HJHJJHJ</li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <div class="text-center"id="harga">
+                        1
+                    </div>
+                </div>
+            </div>
+            <div id="mk2" style="padding:5px" class="row mt-2">
+                <div class="col-8">
+                    <ul class="list-group">
+                        <li class="list-group">MIE GORENG</li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <div class="text-center" id="harga">
+                        1
+                    </div>
+                </div>
+            </div>
+         </div>
+
+         <div class="mx-3 mt-2">
+         <h5><font color="grey">MINUMAN</font></h5>
+            <div id="min1" style="padding:5px" class="row mt-2">
+                <div class="col-8">
+                    <ul class="list-group">
+                        <li class="list-group">AIR MINERAL</li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <div class="text-center" id="harga">
+                        1
+                    </div>
+                </div>
+            </div>
+            <div id="min2" style="padding:5px" class="row mt-2">
+                <div class="col-8">
+                    <ul class="list-group">
+                        <li class="list-group">FANTA</li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <div class="text-center" id="harga">
+                        1
+                    </div>
+                </div>
+            </div>
+         </div>
+
+         <div class="mx-3 my-3">
+         <a id="button" href="#" class="btn btn-success btn-lg btn-block"> <font size="2">SELESAI</font></a>
+      
+         </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.js"
+    integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+    crossorigin="anonymous"></script>
+    <script src="<?=base_url('kitchen.js')?>">
+    </script>
+  </body>
+</html>
