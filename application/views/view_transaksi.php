5410d2512ce41acd46f9e9fc0d5ba66868d8f236
yurizza <44933955+yurizza@users.noreply.github.com>
2020-01-07 14:28:36 +0700

    Update view_transaksi.php

--- application/views/view_transaksi.php
+++ application/views/view_transaksi.php
@@ -5,20 +5,91 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- <style>
+      body {margin:0;}
+
+      .navbar {
+        overflow: hidden;
+        background-color: #FBFBFB;
+        position: fixed;
+        top: 0;
+        width: 100%;
+        padding:0;
+        }
+
+      .navbar a {
+        /* float: left; */
+        display: block;
+        color: #FBFBFB;
+        /* text-align: center; */
+        /* padding: 14px 16px; */
+        text-decoration: none;
+        font-size: 17px;
+        
+      }
+
+      .navbar a:hover {
+        background: #FBFBFB;
+        color: #FBFBFB;
+      }
+
+      .main {
+        /* padding: 16px; */
+        margin-top: 70px;
+        height: auto; /* Used in this example to enable scrolling */
+      }
+    </style> -->
+    <style>
+    body {margin:0;}
+
+    .bawah {
+      overflow: hidden;
+      background-color: white;
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+    }
+
+    .bawah a {
+      float: left;
+      display: block;
+      /* color: #f2f2f2; */
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      font-size: 17px;
+    }
+
+    .bawah a:hover {
+      background: #ddd;
+      color: black;
+    }
+
+    .main {
+      /* padding: 16px; */
+      margin-bottom: 30px;
+      height: auto; /* Used in this example to enable scrolling */
+    }
+    </style>
     <title>restoku</title>
+
 </head>
 <body>
-
-    <nav class="navbar navbar-dark bg-primary">
+<div style="background-color:#FBFBFB">
+    <!-- <div class="navbar"> -->
+    <nav class="navbar navbar-dark bg-primary shadow-sm p-2 my-1 rounded mx-1">
       <a class="navbar-brand">
-        <img src="<?php echo base_url(); ?>/img/Restoku.png" width="150">
+        <img src="<?php echo base_url(); ?>/img/LogoPutih.png" width="120" height="30">
       </a>
-      <form class="form-inline my-2 my-lg-0">
-        <img  id="notif" src="<?php echo base_url(); ?>/img/notif.png" width="20"> 
+        <form class="form-inline my-2 my-lg-0">
+          <img  id="notif" src="<?php echo base_url(); ?>/img/motificationa.png" width="25"> 
         </form>
     </nav>
+    <!-- </div> -->
+ 
+  <div class="main">
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent-5">
-       
         <ul class="navbar-nav ml-auto nav-flex-icons">
           <li class="nav-item avatar dropdown">
             <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
@@ -33,56 +104,46 @@
           </li>
         </ul>
       </div>
+      <!-- gambar resto -->
+     <div class="mt-2">
+            <center>
+                <img class="shadow-sm p-2 bg-white rounded " src="<?php echo base_url(); ?>/img/download.png" width="80">
+            </center>
+     </div>
 
-    <!-- <section> -->
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container p-3">
-        <center>
-          <!-- <canvas id="myCanvas"> -->
-            <img id="MyCanvas"  src="<?php echo base_url(); ?>/img/download.png" width="100">
-          <!-- </canvas> -->
-        </center>
+      <div class="shadow-sm p-3 mb-2 bg-white rounded mt-2 mx-1">
+          <input type="hidden" id="id_a" value="<?=$hasi['id']?>">
+            <div class="container p-3">
+              <table  class="table" >
+                <thead id="hasil"></thead>
+                <tbody id="hasil2"></tbody>
+              </table>
+            </div>
       </div>
+
+      <div class="p-3" style="background-color:#E0FFFF">
+        <center><p> <br><br>Ads<br><br></p></center>
+      </div>
+      <!-- akhir main -->
     </div>
-    <input type="hidden" id="id_a" value="<?=$hasi['id']?>">
-    <div class="container p-3">
-      <table  class="table" >
-      <thead id="hasil">
-      </thead>
-      <tbody id="hasil2">
-      
-      </tbody>
-      </table>
-    </div>
-    <hr>
-    <div class="container p-3 active">
-      <center><p>Ads</p></center>
-    </div>
-      
-    <div class="row p-3">
+ 
+    <div class="bawah">
+    <div class="row my-3">
       <div class="col">
-        <center>
-          <a class="btn btn-success" href="<?php echo base_url('index.php/Transaksi_penjualan/rate'); ?>">Pesanan Komplit</a>
-        </center>
+        <center><button type="button" class="btn btn-success">Pesanan Komplit</button></center>
       </div>
     </div>
-    <!-- </section> -->
+    
+    </div>
+    <!-- akhir bawah  -->
+  </div>
+
+ 
     <script
     src="https://code.jquery.com/jquery-3.4.1.js"
     integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
     crossorigin="anonymous"></script>
-    <script src="<?=base_url('transaksi.js')?>"></script>
-    <!-- <script type="text/javascript">
-      var canvas = dokument.getElementById('MyCanvas');
-      var context = canvas.getContext('2d');
-
-      context.rect(188,40,200,100);
-      context.fillstyle = 'aqua';
-      context.shadowColor = '#999';
-      context.shadowBlur = '5';
-      context.shadowOffSetX = '5';
-      context.shadowOffSetY = '5';
-      context.fill();
-    </script> -->
+      <script src="<?=base_url('transaksi.js')?>">
+        </script>
 </body>
 </html>
