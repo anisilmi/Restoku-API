5589dd11ae7450ac61b8b22a402f5ea3eb558983
Anisa Nabila Imada Silmi <59159579+anisilmi@users.noreply.github.com>
2020-01-09 14:51:40 +0700

    Delete kitchen.js

--- kitchen.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('.row').click(function(){
-    //    alert( $(this).attr('id') );
-      let ids= $(this).attr('id');
-      $("#"+ids).css("background-color","#CCFFCC");
-    });
- });
